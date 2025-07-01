bbfce65799d00a57f8c5146a925288d5cbab014b
Elchin Aliyev <elchineliyevv@gmail.com>
2025-07-01 15:54:54 +0400

    m

--- accounts/models.py
+++ accounts/models.py
@@ -5,10 +5,4 @@ class User(AbstractUser):
     image = models.ImageField(upload_to='images',null=True,blank=True)
     bio = models.TextField()
  
-function staircase(n) {
-    for (let i = 1; i <= n; i++) {
-        const spaces = ' '.repeat(n - i);
-        const hashes = '#'.repeat(i);
-        console.log(spaces + hashes);
-    }
-}
+ 
